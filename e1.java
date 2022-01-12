472eeab09785bcde2af9e2d58cc4f5c2b1392c6f
Твоя Булочка <85406297+Awl-S@users.noreply.github.com>
2022-01-12 21:57:45 +0700

    Update e1.java

--- e1.java
+++ e1.java
@@ -3,16 +3,19 @@ public class Main {
     public static void main(String[] args) {
 
         int x = 20;
-        int arr[];
-        arr = new int[x];
+        int arr[][];
+        arr = new int[x][x];
 
         int max = 1, min = -1;
         int range = max-min+1;
 
+		for(int j = 0; j < x; j++){
         for (int i = 0; i < x; i++){
-            arr[i] = (int)(Math.random()*range)+min;
-            System.out.print(arr[i] + " ");
+            arr[j][i] = (int)(Math.random()*range)+min;
+            System.out.print(arr[j][i] + "\t");
         }
+        System.out.println();
+    }
     }
 
 }
